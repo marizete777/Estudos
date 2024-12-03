20bb93266c4e4358e34246b5b8ea2bc336772feb
marizete777 <marianecardoso278@gmail.com>
2024-12-03 15:08:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Quais são as bases nitrogenadas?',
+    'Atenina (A), Timina (T), Citosina (C), Guanina (G)'
+)
+
+criaCartao(
+    'História',
+    'Quando começou a Ditadura Militar no Brasil?',
+    '1964'
+)
+
+criaCartao(
+    'Ballet',
+    'Onde o ballet surgiu?',
+    'Itália'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
